5c433a2ef13df0dfb0b3fdc3e8d0b19060748891
Enzoxxx <48193335+Enzoxxx@users.noreply.github.com>
2019-04-15 10:13:48 +0800

    Add files via upload

--- FinDB_2019-master/importdata.R
+++ FinDB_2019-master/importdata.R
@@ -29,16 +29,18 @@ etf4.csv<-read.csv("ETF4_2000_2018_d.csv", fileEncoding='big5',
 head(etf4.csv)
 str(etf4.csv)
 # using read_csv to imoprt data to tibble format
-#install.packages("readr")
-library(readr)
+# install.packages("readr")
+# library(readr)
+ifelse(!require(readr), install.packages('readr'), library(readr))
+#
 etf4_csv<-read_csv("ETF4_2000_2018_d.csv")
 # you will get garbled text!
 etf4_csv<-read_csv("ETF4_2000_2018_d.csv", locale = locale(encoding='big5'))
 head(etf4_csv)
 str(etf4_csv)
 # read xls file
-#install.packages("readxl")
-library(readxl)
+ifelse(!require(readxl), install.packages('readxl'), library(readxl))
+#
 etf4_xls<-read_excel("ETF4_2000_2018_d.xls", 
                      col_types =c("text", "text","text", "numeric","numeric"))
 head(etf4_xls)
@@ -48,7 +50,103 @@ head(etf4_xls)
 #-----------------------------------------------------------------------------
 tw50_2017<-read_csv("2017Q4_code.csv", locale = locale(encoding='big5'))
 head(tw50_2017)
+#
+ifelse(!require(quantmod), install.packages('quantmod'), library(quantmod))
+#
+code50<-tw50_2017$code
+code50.tw <- paste(code50, ".TW", sep="")
+code50.tw
+#
+#tickers<-c("2330.TW", "1101.TW", "1102.TW")
+#getSymbols(tickers, from= '2016-01-01', auto.assign = T)
+#
+data=new.env()
+getSymbols(code50.tw, from= '2016-01-01', env = data, auto.assign = T)
+names(data)
+data$`1101.TW`
+# Error: 2311.TW download failed after two attempts. Error message:
+# Replace 2311 with 3711
+getSymbols("3711.TW", from= '2016-01-01', auto.assign = T)
+data$"3711.TW"<-`3711.TW`
+# Combine adjusted closing price across all 50 stocks!
+str(data)
+ls(data)
+tw50p<-data.frame()
 
+t<-as.data.frame(as.list(data))
+dim(t)
+head(t)
+head(t[1])
+
+for (i in c("1101.TW", "1102.TW")) {
+    tmp<-paste(data)
+    tw50p<-cbind(tw50p, Ad(data$"1101.TW"))
+}
+#--------------------------------------------------
+# 錯誤處理：
+# Reference: 
+# tryCatch()
+# https://www.cnblogs.com/weibaar/p/4382397.html
+# http://xuyt.blogspot.com/2013/10/rtry-and-catch.html
+# try()
+# http://www.endmemo.com/program/R/try.php
+# inherits()
+# http://www.learn-r-the-easy-way.tw/chapters/5
+#---------------------------------------------------
+# use loop to solve error problem
+# i=1
+# for(i in 1:length(code50.tw)) {
+#   symbol <- code50.tw[i]
+#   tryit <- try(getSymbols(symbol,from="2016-01-01", src='yahoo'))
+#   # specify the "from" date to desired start date
+#   if(inherits(tryit, "try-error")){
+#     i <- i+1
+#   } else {
+#     data <- getSymbols(symbol, from="2016-04-27", src='yahoo')# specify the "from" date to desired start date
+#     dataset <- merge(dataset, Cl(get(name[i])))#將所有股票的收盤價 Cl 合併成一個 data frame
+#     rm(symbol)
+#   }
+# }
+#------------------------------------------------------------------------------
+# 將沒有找到的股票error找出，並將結果輸出為NULL
+show_condition <- function(code){
+  tryCatch(code, 
+           error = function(c){print("error"); return(NULL)},
+           warning = function(c){print(paste("Caught warning message:", symboli))},
+           message = function(c){symboli}
+           )
+}
+# 將沒有找到的股票error找出，並將結果輸出為NULL
+all.data<-c()
+TSE_tw50_symbols_yahoo<-c()
+#
+symboli = "1101.TW"
+for (symboli in code50.tw) {
+  yx = show_condition(getSymbols(symboli, from = '2016-01-01', index.class = 'Date'))
+  Sys.sleep(0.2 + runif(1)/2) # 不要一直抓，讓系統短暫休息
+  if(is.null(yx)){
+    for (hx in seq(1)){
+      yx = show_condition(getSymbols(symboli, from = '2016-01-01', index.class = 'Date'))
+      Sys.sleep(0.2 + runif(1)/2) # 不要一直抓，讓系統短暫休息
+      print(paste("重試", hx, "次"))
+    }
+   }
+  if(!is.null(yx)){ #無錯誤就繼續抓
+    print(c(i, symboli))
+    getSymbols(symboli, from = '2016-01-01', index.class = 'Date')
+    all.data<-cbind(all.data, get(symboli))
+    TSE_tw50_symbols_yahoo<-cbind(TSE_tw50_symbols_yahoo, yx)
+    }
+}
+
+
+all.data
+dim(all.data)
+head(all.data)
+tw50<-Cl(all.data)
+head(tw50)
+tw50<-na.omit(tw50)
+tw50
 #=============================================================================
 # clean data
 etf4.c<-etf4_csv[, c(-2, -4)]
@@ -81,6 +179,7 @@ etf4.xts<-xts(etf4.reorder[,-1], order.by = etf4.reorder$date)
 head(etf4.xts)
 tail(etf4.xts)
 str(etf4.xts)
+saveRDS(etf4.xts, "etf4_xts_all")
 #----------------------------------------------
 # Handling missingness in your data 
 #----------------------------------------------
@@ -109,10 +208,10 @@ head(lag_x)
 #-----------------------------------------------------------
 # export data
 #----------------------------------------------------------
-write.csv(etf4.xts, file = "myetf4.csv")
+write.csv(etf4.xts1, file = "myetf4.csv")
 # date index disappears!!!
 # you have to use write.zoo to save .xts file
-write.zoo(etf4.xts, sep = ',', file = "myetf4.csv.1")
+# write.zoo(etf4.xts, sep = ',', file = "myetf4.csv.1")
 saveRDS(etf4.xts, file = "etf4.xts.rds")
 etf4.xts2 <- readRDS("etf4.xts.rds")
 head(etf4.xts2)
@@ -129,12 +228,6 @@ head(etf4.xts3)
 etf4_2016<-etf4.xts['2016']
 etf4_2016_01_06 <- etf4.xts["20160101/20160630"]
 head(etf4_2016_01_06)
-#
-lastweek <- last(etf4_2016, "1 week")
-# Print the last 2 observations in lastweek
-last(lastweek, 2)
-# Extract all but the first two days of lastweek
-first(lastweek, "-2 days")
 
 #------------------------------------------------------------
 # Converting Daily Prices to Monthly Returns in the xts world
@@ -171,6 +264,8 @@ plot(etf4_returns_xts, xaxt='n')
 axis(1, index(etf4_returns_xts), format(index(etf4_returns_xts), "%Y/%m"))
 # plot the scatterplot of 0050 and 00646
 # convert xts into df using fortify()
+library(ggplot2)
+#
 etf4_ret.df1<-fortify(etf4_returns_xts)
 head(etf4_ret.df1)
 plot(etf4_ret.df1$`0050`, etf4_ret.df1$`00646`, pch=20,
@@ -200,24 +295,28 @@ etf4_ret
 etf4_ret.tmp<-data.frame(date = index(etf4_returns_xts), etf4_ret)
 head(etf4_ret.tmp)
 # or you can use the following code
-etf4_ret.tmp<-etf4_returns_xts %>% 
+# %>% pipe operator
+#etf4_ret.tmp<-data.frame(etf4_returns_xts, date=index(etf4_returns_xts)) 
+
+etf4_ret.tmp02<-etf4_returns_xts %>% 
   data.frame(date=index(.)) %>% 
   remove_rownames() %>% 
   gather(asset, return, -date) # turn data into long format
 
-head(etf4_ret.tmp)
+head(etf4_ret.tmp02)
 #
-plot(etf4_ret.tmp$X0050, etf4_ret.tmp$X0056)
+#plot(etf4_ret.tmp02$X0050, etf4_ret.tmp02$X0056)
 #
-ggplot(etf4_ret.tmp) +
-  geom_point(mapping = aes(x = etf4_ret.tmp$`0050`, y = etf4_ret.tmp$`0056`))
+#ggplot(etf4_ret.tmp02) +
+#  geom_point(mapping = aes(x = X0050, y = X0056))
 #
-
+#---------------------------------------------------
 etf4_ret.df<-fortify(etf4_returns_xts, melt=TRUE)
 head(etf4_ret.df)
 #
 p<-ggplot(etf4_ret.df, aes(x = Index, y = Value))+
   geom_line(aes(color = Series), size = 1)
+p
 
 p + scale_x_date(date_labels = "%Y/%m")
 
@@ -251,9 +350,11 @@ etf4_ret.df %>%
   theme_update(plot.title = element_text(hjust = 0.5))
 
 #---------------------------------------------------------------
-
-
-
+library(plotly)
+p1<-plot_ly(etf4_ret.tmp, x = ~date, y=~X0050, name = "0050", type = 'scatter', mode = 'lines') %>% 
+    add_trace(y=~X0056, name = '0056', mode = 'lines+markers') %>% 
+    layout(xaxis = list(title ='year'), yaxis = list(title='monthly returns'))
+p1
 
 
 
